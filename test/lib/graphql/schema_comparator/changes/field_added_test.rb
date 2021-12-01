4477b8ddcd6a77678d1d683cd43af0489fb3261a
Sam Bostock <sam.bostock@shopify.com>
2021-11-30 22:58:12 -0500

    Add test for existing field addition criticality

---
+++ test/lib/graphql/schema_comparator/changes/field_added_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+class GraphQL::SchemaComparator::Changes::FieldAddedTest < Minitest::Test
+  def test_criticality_is_non_breaking_by_default
+    change = GraphQL::SchemaComparator::Changes::FieldAdded.new(
+      GraphQL::ObjectType.define { name "Type" },
+      GraphQL::Field.define { name "field" },
+    )
+
+    assert change.non_breaking?
+  end
+
+  def test_criticality_is_non_breaking_for_id_field_addition
+    change = GraphQL::SchemaComparator::Changes::FieldAdded.new(
+      GraphQL::ObjectType.define { name "Type" },
+      GraphQL::Field.define { name "id" },
+    )
+
+    assert change.non_breaking?
+  end
+end
